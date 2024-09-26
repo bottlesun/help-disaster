68a49d99df88d7936a7c718c72eec65956ac551e
bottlesun <bottlesun3514@gmail.com>
2024-09-26 15:30:29 +0900

    fix : date 오류 , forwardRef 방식 변경

--- next/app/_components/common/box/BoxRefreshDate.tsx
+++ next/app/_components/common/box/BoxRefreshDate.tsx
@@ -1,12 +1,10 @@
 import { ItemRefreshDate } from "@/_components/common/box/box.style";
 import { IoIosRefresh } from "react-icons/io";
 import dayjs, { Dayjs } from "dayjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const BoxRefreshDate = () => {
-  const [date, setDate] = useState<Dayjs | null | string>(
-    dayjs().format("YYYY/MM/DD A HH:mm:ss"),
-  );
+  const [date, setDate] = useState<Dayjs | null | string>(null);
 
   const onClickDateRefresh = () => {
     setDate(dayjs().format("YYYY/MM/DD A HH:mm:ss"));
@@ -15,6 +13,10 @@ const BoxRefreshDate = () => {
     // if (pageNo === 1) return;
     // setPageNo(1);
   };
+
+  useEffect(() => {
+    setDate(dayjs().format("YYYY/MM/DD A HH:mm:ss"));
+  }, []);
 
   return (
     <div className={ItemRefreshDate} onClick={onClickDateRefresh}>
--- next/app/_components/disaster/HomeDisaster.tsx
+++ next/app/_components/disaster/HomeDisaster.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ForwardedRef } from "react";
+import React, { ForwardedRef, forwardRef } from "react";
 import { DisasterMessageData } from "@/_types/disaster-message.api.type";
 import { removeTrailingValues } from "@/_utils/textFilter";
 import {
@@ -13,8 +13,8 @@ import DisasterTitle from "@/_components/disaster/DisasterTitle";
 import BoxTitle from "@/_components/common/box/BoxTitle";
 import SpinnerView from "@/_components/common/spinner/spinner.view";
 import BoxEmpty from "@/_components/common/box/BoxEmpty";
-import Scrollbars from "react-custom-scrollbars-2";
 import dynamic from "next/dynamic";
+import Scrollbars from "react-custom-scrollbars-2";
 
 const ScrollbarsComponent = dynamic(() => import("react-custom-scrollbars-2"), {
   ssr: false,
@@ -27,50 +27,53 @@ interface HomeDisasterProps {
   handleTopScroll?: () => void;
 }
 
-const HomeDisaster = (
-  { data, isLoading, handleScroll, handleTopScroll }: HomeDisasterProps,
-  ref: ForwardedRef<Scrollbars>,
-) => {
-  // const res = await fetch("/api/disaster");
-  // console.log(data);
-  return (
-    <section className={ItemContainer}>
-      <DisasterTitle isLoading={false} dataLength={data.length} />
-      <ScrollbarsComponent
-        ref={ref}
-        autoHide
-        universal
-        autoHideTimeout={1000}
-        autoHideDuration={300}
-        autoHeight
-        autoHeightMax={"calc(100vh - 330px)"}
-        onScroll={handleScroll}
-      >
-        <div className={ItemContent}>
-          {data?.length === 0 && <BoxEmpty />}
-          {data?.length !== 0 && isLoading && <SpinnerView />}
-          {data?.length !== 0 &&
-            data?.map((item) => {
-              return (
-                <div className={ItemBox} key={item.SN}>
-                  <BoxTitle
-                    text={`[${item.EMRG_STEP_NM}] ${item.DST_SE_NM} 발신 메시지`}
-                  />
+const HomeDisaster = forwardRef(
+  (
+    { data, isLoading, handleScroll, handleTopScroll }: HomeDisasterProps,
+    ref: ForwardedRef<Scrollbars>,
+  ) => {
+    // const res = await fetch("/api/disaster");
+    // console.log(data);
+    return (
+      <section className={ItemContainer}>
+        <DisasterTitle isLoading={false} dataLength={data?.length || 0} />
+        <ScrollbarsComponent
+          ref={ref}
+          autoHide
+          universal
+          autoHideTimeout={1000}
+          autoHideDuration={300}
+          autoHeight
+          autoHeightMax={"calc(100vh - 330px)"}
+          onScroll={handleScroll}
+        >
+          <div className={ItemContent}>
+            {data?.length === 0 && <BoxEmpty />}
+            {data?.length !== 0 && isLoading && <SpinnerView />}
+            {data?.length !== 0 &&
+              data?.map((item) => {
+                return (
+                  <div className={ItemBox} key={item.SN}>
+                    <BoxTitle
+                      text={`[${item.EMRG_STEP_NM}] ${item.DST_SE_NM} 발신 메시지`}
+                    />
 
-                  <p className={ItemText}>
-                    {removeTrailingValues(item.MSG_CN)}
-                  </p>
-                  <div className={ItemLocation}>
-                    <p>{item.RCPTN_RGN_NM} </p>
-                    <p>{item.REG_YMD}</p>
+                    <p className={ItemText}>
+                      {removeTrailingValues(item.MSG_CN)}
+                    </p>
+                    <div className={ItemLocation}>
+                      <p>{item.RCPTN_RGN_NM} </p>
+                      <p>{item.REG_YMD}</p>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-        </div>
-      </ScrollbarsComponent>
-    </section>
-  );
-};
+                );
+              })}
+          </div>
+        </ScrollbarsComponent>
+      </section>
+    );
+  },
+);
+HomeDisaster.displayName = "HomeDisaster";
 
 export default HomeDisaster;
